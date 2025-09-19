93d08d6d7b55abd2479a9fbd6e7a67c4a406542c
Lesya <117307391+Lesyalys@users.noreply.github.com>
2025-09-19 17:32:23 +0500

    new

--- course 3/СУБД/query/Select2.sql
+++ course 3/СУБД/query/Select2.sql
@@ -3,10 +3,11 @@ select PAYMENTFIO
 , b.BRANDNAME
 ,a.PRICE
 ,a.MILEAGE
-,a.STATUS
+,s.STATUS_NAME
 from PAYMENT
 left join AUTO a on a.id = PAYMENT.PAYMENTCAR_ID
 left join CONTACT c on c.id = PAYMENT.CONTACT_ID
 left join STATUSCODE s on a.STATUS = s.id
 left join BRAND b on a.BRAND_ID  = b.id
+where s.id = 1
 order by PAYMENTFIO asc
