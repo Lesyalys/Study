c519ebcbe550aa62e0cac8558a9d7ffe815e8aa7
Lesya <117307391+Lesyalys@users.noreply.github.com>
2025-10-28 12:00:03 +0500

    fix:

--- course 3/Визуальные среды программирования/labs/labs1/labs1/labs1/obj/Debug/net8.0-windows/labs1.AssemblyInfo.cs
+++ course 3/Визуальные среды программирования/labs/labs1/labs1/labs1/obj/Debug/net8.0-windows/labs1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("labs1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08d8fcfe43a4e67f7bc80e710d851f15cb7b364e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70a97a33d57d3783608363dfcbf10737e2df2c2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("labs1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("labs1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
