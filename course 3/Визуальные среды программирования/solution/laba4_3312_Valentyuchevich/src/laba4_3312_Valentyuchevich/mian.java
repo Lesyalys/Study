bee72191f45b2c6d39541979fca46b6558e8b8d2
Lesya <117307391+Lesyalys@users.noreply.github.com>
2025-10-29 11:01:48 +0500

    fix:

--- course 3/Визуальные среды программирования/solution/laba4_3312_Valentyuchevich/src/laba4_3312_Valentyuchevich/mian.java
+++ course 3/Визуальные среды программирования/solution/laba4_3312_Valentyuchevich/src/laba4_3312_Valentyuchevich/mian.java
@@ -11,6 +11,7 @@ public class mian {
 	public static void main(String[] args) {
 		JFrame fr=new JFrame("Вращение треугольника вокруг своего центра тяжести");
 		fr.setPreferredSize( new Dimension(300,300));
+		
 		final JPanel pan= new JPanel();
 		fr.add(pan);
 		fr.setVisible(true);
@@ -21,15 +22,17 @@ public class mian {
 		int i=0;
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-		Graphics2D gr=(Graphics2D)pan.getRootPane().getGraphics();
-		pan.update(gr);
-		GeneralPath path=new GeneralPath();
-		path.append(new Polygon(new int []{60,-80,50},new int[]{-60,-50,40},3),true);
-		int x=(60-80+50)/3,y=(-60-50+40)/3;
-		gr.translate(150, 150);
-		AffineTransform tranforms = AffineTransform.getRotateInstance((i++)*0.07, x, y);
-		gr.transform(tranforms);
-		gr.draw(path);
+			
+			Graphics2D gr=(Graphics2D)pan.getRootPane().getGraphics();
+			pan.update(gr);
+			GeneralPath path=new GeneralPath();
+			
+			path.append(new Polygon(new int []{60,-80,50},new int[]{-60,-50,40},3),true);
+			int x=(60-80+50)/3,y=(-60-50+40)/3;
+			gr.translate(150, 150);
+			AffineTransform tranforms = AffineTransform.getRotateInstance((i++)*0.07, x, y);
+			gr.transform(tranforms);
+			gr.draw(path);
 		}});
 		tm.start();
 
