f38dd3aa5a224a71da9ef44d077eeee364c615d8
Lesya <117307391+Lesyalys@users.noreply.github.com>
2025-11-08 21:04:52 +0500

    new

---
+++ course 3/Визуальные среды программирования/2-6_java/laba6_3312_Valentyuchevich/src/laba5_3312_Valentyuchevich/Main.java
@@ -0,0 +1,36 @@
+package laba5_3312_Valentyuchevich;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("=== Starting Client-Server Application ===");
+        
+        // Инициализация сетевых настроек
+        PortSetting networkSettings = new PortSetting();
+        
+        System.out.println("\n--- Server Side ---");
+        Server server = new Server();
+        server.displayServerInfo();
+        
+        // в отдельном потоке
+        Thread serverThread = new Thread(() -> {
+            try {
+                server.startServer();
+            } catch (Exception e) {
+                System.out.println("Server error: " + e.getMessage());
+            }
+        });
+        serverThread.start();
+        
+        //время на запуск
+        Thread.sleep(2000);
+        
+        System.out.println("\n--- Client Side ---");
+        Client client = new Client();
+        client.displayClientInfo();
+        client.startClient();
+        
+        // завершения работы
+        serverThread.join();
+        System.out.println("\n=== Application Finished ===");
+    }
+}
